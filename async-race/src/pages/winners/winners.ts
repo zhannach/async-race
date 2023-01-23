bfe3afc38fa76c67c0fff33b976eec243a221778
Zhanna <zhanna01992@gmail.com>
2023-01-23 23:32:48 +0200

    feat: update winners page

--- async-race/src/pages/winners/winners.ts
+++ async-race/src/pages/winners/winners.ts
@@ -3,61 +3,74 @@ import { CarType, Winner } from "../type/types"
 import interpolate from '../helpers/interpolate'
 import { createEl } from "../helpers/functions"
 import Garage from "../garage/garage"
+import { Pagination } from "../helpers/pagination"
 
 export default class Winners {
   api
+  garage
   root: HTMLElement
   winners: Winner[] = []
+  count: number = 0
   pageNumber: number = 1
+  pagination: Pagination | undefined
+  winnersEl: HTMLElement
   carsWinnerEl: HTMLElement
-  carWinnerPagination: HTMLElement
   carsEl: CarType[] | undefined
 
 
   constructor(api: CarApi, garage: Garage) {
     this.api = api
+    this.garage = garage
     this.root = garage.root
+    this.winnersEl = createEl('section', 'winners')
     this.carsWinnerEl = createEl('section', 'cars-winners')
+    this.winnersEl.append(this.carsWinnerEl)
     this.carsEl = garage.items
-    this.carWinnerPagination = garage.paginationEl
-    this.root.append(this.carsWinnerEl, this.carWinnerPagination)
+    if (window.location.pathname == '/winners' && window.location.hash.slice(0, 5) === '#page') {
+      this.pageNumber = Number(window.location.hash.slice(5))
+    }
   }
 
   async run() {
-    await this.renderWinners()
+    await this.renderWinners(this.pageNumber)
+    if(!this.pagination) this.pagination = new Pagination(
+      this.winnersEl,
+      this.pageNumber,
+      this.count,
+      (pageNumber: number) => this.renderWinners(pageNumber)
+    )
+    this.root.append(this.winnersEl)
   }
 
-  async renderWinners() {
+  async renderWinners(pageNumber: number) {
     const template = document.querySelector('#winner') as HTMLTemplateElement
-    const { winners, count } = await this.api.getWinners(this.pageNumber, 10, 'time', 'ASC')
+    const { winners, count } = await this.api.getWinners(pageNumber, 10, 'time', 'ASC')
     this.winners = winners
-    console.log( this.winners)
+    this.count = Number(count)
     this.carsWinnerEl.innerHTML = `<h2 class="cars-winners__title">WINNERS
     <span class="cars-winners__amount">(${count})</span>
   </h2>
   <h3 class="cars-winners__subtitle">Page
-    <span class="cars-winners__page">#${this.pageNumber}</span>
+    <span class="cars-winners__page">#${pageNumber}</span>
   </h3>
   <section class="cars-winners">
   <table class="cars-winners__table">`
-  
+
     const winnersTable = this.carsWinnerEl.querySelector('.cars-winners__table') as HTMLTableElement
-      const winnersCarElements = this.winners.map((winner, index) => {
-        if (!winner.car) return
-        return interpolate(template.innerHTML, { winner, index})
-      })
-      winnersTable.innerHTML = `<tr class="winners-table__titles">
+    const winnersCarElements = this.winners.map((winner, index) => {
+      if (!winner.car) return
+      return interpolate(template.innerHTML, { winner, index })
+    })
+    winnersTable.innerHTML = `<tr class="winners-table__titles">
       <th>Number</th>
       <th>Car</th>
       <th>Name</th>
       <th>Wins</th>
       <th>Best time(seconds)</th>
     </tr>
-      ${winnersCarElements.join('') } `
-    //   carWinnerPagination.innerHTML = ` < div class="pagination-btns" >
-        //   <button class="pagination-btn pagination-btn__prev">PREV</button>
-        //   <button class="pagination-btn pagination-btn__next">NEXT</button>
-        // </>`
-      this.carsWinnerEl.append(winnersTable)
-    }
+      ${winnersCarElements.join('')} `
+    this.carsWinnerEl.append(winnersTable)
+  }
+
+
 }
